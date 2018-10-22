a44d9442bdd91634a7a640944fc0784ba63eb221
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-10-22 08:05:19 -0400

    Initialized the assembly language instructions and added instructions to print @

--- MIPS_Project_1.s
+++ MIPS_Project_1.s
@@ -2,3 +2,8 @@
 # @02840580
 # Gautam, Anmol
 # value in register is updated from the initial register value every time
+.text                    # Assembly language instructions
+main:                    # Start of the code section
+    li $v0,  11          # system call code for printing character = 11
+    li $a0, 64           # load address of the character '@' to be printed in $a0
+    syscall
