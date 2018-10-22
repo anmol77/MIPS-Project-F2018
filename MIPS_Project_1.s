206c2cc2c207665c8b46ed85b242ca17b1412079
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-10-22 08:21:02 -0400

    Added instructions to print a space in output

--- MIPS_Project_1.s
+++ MIPS_Project_1.s
@@ -55,3 +55,6 @@ main:                    # Start of the code section
     li $v0,  11          # system call code for printing character = 11
     addi, $a0, $a0, -65  # loads address of character ',' to be printed in $a0
     syscall
+    li $v0,  11          # system call code for printing character = 11
+    addi, $a0, $a0, -12  # loads address of character space to be printed in $a0
+    syscall
