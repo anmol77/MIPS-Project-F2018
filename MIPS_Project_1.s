76da15eb9688e93e369ab5fcee749322ef768095
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-10-22 08:18:10 -0400

    Added instructions to print character u in output

--- MIPS_Project_1.s
+++ MIPS_Project_1.s
@@ -40,3 +40,6 @@ main:                    # Start of the code section
     li $v0,  11          # system call code for printing character = 11
     addi, $a0, $a0, 26   # loads address of character a to be printed in $a0
     syscall
+    li $v0,  11          # system call code for printing character = 11
+    addi, $a0, $a0, 20   # loads address of character u to be printed in $a0
+    syscall
