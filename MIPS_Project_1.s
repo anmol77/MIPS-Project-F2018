8d1b50e48a231bfe364faabfd490329d435f39ab
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-10-22 08:17:21 -0400

    Added instructions to print character G in output

--- MIPS_Project_1.s
+++ MIPS_Project_1.s
@@ -34,3 +34,6 @@ main:                    # Start of the code section
     li $v0,  11          # system call code for printing character = 11
     addi $a0, $a0, 10    # loads address of character newline to be printed in $a0
     syscall
+    li $v0,  11          # system call code for printing character = 11
+    addi, $a0, $a0, 61   # loads address of character G to be printed in $a0
+    syscall
