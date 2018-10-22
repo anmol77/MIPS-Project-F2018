5face8b99fec94d961689ff4c37b49c2e93c0491
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-10-22 08:13:49 -0400

    Added instruction to print integer 0 of student id

--- MIPS_Project_1.s
+++ MIPS_Project_1.s
@@ -19,3 +19,6 @@ main:                    # Start of the code section
     li $v0, 1            # system call code for printing integer value = 1
     addi $a0, $a0, -4    # loads address of integer 4 to be printed in $a0
     syscall
+    li $v0, 1            # system call code for printing integer value = 1
+    addi $a0, $a0, -4    # loads address of integer 0 to be printed in $a0
+    syscall
