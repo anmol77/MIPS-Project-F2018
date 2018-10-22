1f485b923a67dbf2baa4f4f9addafb9fffc5f840
anmol gautam <anmolgautam@anmols-MacBook-Pro.local>
2018-10-22 08:23:45 -0400

    Added instructions to print character l of my first name in output

--- MIPS_Project_1.s
+++ MIPS_Project_1.s
@@ -70,3 +70,6 @@ main:                    # Start of the code section
     li $v0,  11          # system call code for printing character = 11
     addi, $a0, $a0, 2    # loads address of character o to be printed in $a0
     syscall
+    li $v0,  11          # system call code for printing character = 11
+    addi, $a0, $a0, -3   # loads address of character l to be printed in $a0
+    syscall
